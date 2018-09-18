9c8c5aeae05f6829564b0d15d30f4bfeb6347e22
Nicholas Alvarez <38587876+nicholasaalvarez@users.noreply.github.com>
2018-09-18 08:30:34 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,11 @@
 //'config.js' is ignored by git to protect sensitive information, such as your database's username and password
 //copy this file's contents to another file 'config.js' and store your MongoLab uri there
 
+
+//Changed uri to what pedro defined for username and password
 module.exports = {
   db: {
-    uri: 'mongodb://nicholasaalvarez:gators38@ds259732.mlab.com:59732/nickalvarez', //place the URI of your mongo database here.
+    uri: 'mongodb://CEN3031:CEN3031TA@ds259732.mlab.com:59732/nickalvarez', //place the URI of your mongo database here.
   }
 };
 
